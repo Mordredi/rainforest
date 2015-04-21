50cd87c74ef541fcce4d735b75a56b3ccf84a418
Arthur <arthur.p.wright@gmail.com>
2015-04-21 15:13:24 -0400

    Added User MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products, :users
   root 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
