9f6805b04e9c768b5647a5918323032e031bbc1d
Arthur <arthur.p.wright@gmail.com>
2015-04-21 15:57:54 -0400

    Created login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :products, :users
+  resources :sessions, :only => [:create]
   root 'products#index'
+  get '/login' => 'sessions#new'
+  get '/logout' => "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
